976d2a888a89b183ac3147119ee391accb85935c
NuaratJahan <86964644+JahanNila155@users.noreply.github.com>
2021-10-20 17:51:44 +0600

    Add files via upload

---
+++ Task 4/src/nusrat/AdvancedArithmetic.java
@@ -0,0 +1,18 @@
+package nusrat;
+
+import java.math.BigInteger;
+
+public interface AdvancedArithmetic {
+    /*
+    Name : Nusrat Jahan
+    ID: 2012020155
+    Section: 53(D)
+    Email : cse_2012020155@lus.ac.bd
+    Date- 16-10-2021
+     */
+
+    int divisorSum(int n);
+        BigInteger findFactorial(int N);
+    }
+
+
